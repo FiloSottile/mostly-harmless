f134fcda66c4e8d2447dcdbc72c7bc0346c37363
Filippo Valsorda <filippo.valsorda@gmail.com>
2012-12-09 19:30:46 +0100

    new scraping, old one was terrible

--- fetch.py
+++ fetch.py
@@ -7,24 +7,38 @@ import traceback
 from geopy import geocoders  
 g = geocoders.Google(domain='maps.google.com')
 
+from bs4 import BeautifulSoup
+
 def parse_listing(listing):
-    res = {}
-    if '<strong>Event name</strong>' in listing:
-        res['name'] = re.search(r'<strong>Event name</strong>: <b>(?:<[^>]+?>)*([^<]+)(?:<[^>]+?>)*</b>', listing).group(1)
-    else:
-        res['name'] = re.search(r'<li>Name: (?:<[^>]+?>)*([^<]+)', listing).group(1)
-    mobj = re.search(r'Email: <b>([^ ]+) &quot;at"</b>&nbsp;<strong>\n([^&]+)&nbsp;</strong>', listing)
-    res['email'] = str(len(mobj.group(2).split('.')[0])) + ',' + str(len(mobj.group(2))) + ',' + mobj.group(2).replace('.', '', 1) + mobj.group(1) if mobj else None
-    mobj = re.search(r'Fingerprint: <a href="[^"]*">(\w+)<b>(\w+)</b></a>\n \(([\w/]+)\)', listing)
-    res['fingerprint'] = mobj.group(1) + mobj.group(2) if mobj else None
-    res['key'] = mobj.group(2) if mobj else None
-    res['keytype'] = mobj.group(3) if mobj else None
-    mobj = re.search(r'URL: <b><a href="([^"]+)">', listing, re.DOTALL)
-    res['url'] = mobj.group(1) if mobj else None
-    mobj = re.search(r'Key comment: <strong><tt>(.*?)</tt></strong>', listing, re.DOTALL)
-    res['key_comment'] = mobj.group(1) if mobj else None
-    mobj = re.search(r'Notes: <br /><blockquote><tt>(.*?)</tt></blockquote>', listing, re.DOTALL)
-    res['notes'] = mobj.group(1).replace('<br />\r\n', '\n').replace('\r\n', '\n') if mobj else None
+    res = {
+            'name': None,
+            'email': None,
+            'fingerprint': None,
+            'key': None,
+            'keytype': None,
+            'url': None,
+            'key_comment': None,
+            'notes': None
+          }
+    for li in listing.find_all("li", recursive=False):
+        if li.get_text().startswith('Event name:'):
+            res['name'] = li.a.string
+        elif li.get_text().startswith('Name:'):
+            res['name'] = li.a.string
+        elif li.get_text().startswith("Email:"):
+            who = li.b.string.rsplit(" ", 1)[0]
+            at = li.strong.string.strip()
+            res['email'] = str(len(at.split('.')[0])) + ',' + str(len(at)) + ',' + at.replace('.', '', 1) + who
+        elif li.get_text().startswith("Fingerprint:"):
+            res['fingerprint'] = li.a.get_text()
+            res['key'] = li.a.b.string
+            res['keytype'] = re.search(r"\(([^\)]+)\)", li.get_text()).group(1)
+        elif li.get_text().startswith("URL:"):
+            res['url'] = li.a.get("href")
+        elif li.get_text().startswith("Key comment:"):
+            res['key_comment'] = li.tt.string
+        elif li.get_text().startswith("Notes:"):
+            res['notes'] = li.tt.get_text().replace('\r\n', '{br}').replace('\n', ' ').replace('{br}', '\n')
     return res
 
 # TODO Key ID field like here http://biglumber.com/x/web?sn=Enrico+Franceschi
@@ -38,10 +52,10 @@ result = {}
 
 for location in locations_list:
     try:
-        city_listings_page = urllib2.urlopen('http://biglumber.com/x/web?sl=' + location).read().decode('iso-8859-1')
-        location_name = re.search(r'<h1>Biglumber listings for ([^<]+)</h1>', city_listings_page).group(1)
+        city_listings_page = BeautifulSoup(urllib2.urlopen('http://biglumber.com/x/web?sl=' + location))
+        location_name = re.search(r'Biglumber listings for (.+)', city_listings_page.find("h1").string).group(1)
         place, (lat, lng) = g.geocode(location_name.encode('utf8'), exactly_one=False)[0]
-        listings = re.findall(r'^<ul>$(.*?)^</ul>$', city_listings_page, re.MULTILINE | re.DOTALL)
+        listings = city_listings_page.html.body.find_all("ul", recursive=False)
         parsed_listings = map(parse_listing, listings)
         print place, '=>', len(parsed_listings)
         result[location] = (location_name, (lat, lng), len(parsed_listings), parsed_listings)
