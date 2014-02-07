4b16f0c0b1e637ed190efe376d16c5c7db33a85f
Filippo Valsorda <filosottile.wiki@gmail.com>
2014-02-07 02:20:13 +0100

    Use setuser

--- iodined.sh
+++ iodined.sh
@@ -4,4 +4,4 @@
 exec mkdir -p /dev/net
 exec mknod /dev/net/tun c 10 200
 
-exec chpst -u iodined iodined -c -f 10.16.0.1 $IODINE_HOST -P $IODINE_PASSWORD >>/var/log/iodined.log 2>&1
+exec /sbin/setuser iodined iodined -c -f 10.16.0.1 $IODINE_HOST -P $IODINE_PASSWORD >>/var/log/iodined.log 2>&1
