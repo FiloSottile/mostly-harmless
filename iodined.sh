7ce4d8dfbd4242a13e639130b6156c517b376502
Filippo Valsorda <filosottile.wiki@gmail.com>
2014-02-07 03:00:09 +0100

    run as root, as supposed to

--- iodined.sh
+++ iodined.sh
@@ -4,4 +4,4 @@
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 
-exec /sbin/setuser iodined iodined -c -f 10.16.0.1 $IODINE_HOST -P $IODINE_PASSWORD >>/var/log/iodined.log 2>&1
+exec iodined -c -f 10.16.0.1 $IODINE_HOST -P $IODINE_PASSWORD >>/var/log/iodined.log 2>&1
