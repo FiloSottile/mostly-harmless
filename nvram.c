20202367dbb8248a14d96252bd1816f45179046a
Filippo Valsorda <filippo@cloudflare.com>
2014-10-22 17:15:28 +0100

    Error handling and test function

--- nvram.c
+++ nvram.c
@@ -31,225 +31,135 @@ cc -o nvram nvram.c -framework CoreFoundation -framework IOKit -Wall
 #include <mach/mach_error.h>
 
 // Prototypes
-void setup(void);
-void teardown(void);
-static kern_return_t GetOFVariable(char *name, CFStringRef *nameRef,
-                                   CFTypeRef *valueRef);
-static kern_return_t SetOFVariable(char *name, char *value);
-static void DeleteOFVariable(char *name);
-void PrintOFVariables(void);
-static void PrintOFVariable(const void *key,const void *value,void *context);
-
-static void NVRamSyncNow(char *name);
+int Setup(char **error);
+void Teardown(void);
+int Get(char *name, char **value, char **error);
+int Set(char *name, char *value, int length, char **error);
+int Delete(char *name, char **error);
 
 // Global Variables
 static io_registry_entry_t gOptionsRef;
 
 
-void setup()
+int Setup(char **error)
 {
   kern_return_t       result;
   mach_port_t         masterPort;
   
   result = IOMasterPort(bootstrap_port, &masterPort);
   if (result != KERN_SUCCESS) {
-    errx(1, "Error getting the IOMaster port: %s",
-        mach_error_string(result));
+    asprintf(error, "Error getting the IOMaster port: %s", mach_error_string(result));
+    return -1;
   }
   
   gOptionsRef = IORegistryEntryFromPath(masterPort, "IODeviceTree:/options");
   if (gOptionsRef == 0) {
-    errx(1, "nvram is not supported on this system");
+    asprintf(error, "nvram is not supported on this system");
+    return -1;
   }
+
+  return 0;
 }
 
-void teardown()
+void Teardown()
 {
   IOObjectRelease(gOptionsRef);
 }
 
 
-// SetOrGetOFVariable(str)
-//
-//   Parse the input string, then set or get the specified
-//   firmware variable.
-//
-static void SetOrGetOFVariable(char *str)
+int Get(char *name, char **value, char **error)
 {
-  long          set = 0;
-  char          *name;
-  char          *value;
+  CFStringRef   nameRef;
+  CFTypeRef     valueRef;
+  uint32_t      length;
+
+  nameRef = CFStringCreateWithCString(kCFAllocatorDefault, name,
+                                       kCFStringEncodingUTF8);
+  if (nameRef == 0) {
+    asprintf(error, "Error creating CFString for key %s", name);
+    return -1;
+  }
+  
+  valueRef = IORegistryEntryCreateCFProperty(gOptionsRef, nameRef, 0, 0);
+  if (valueRef == 0) return -2; // kIOReturnNotFound
+
+  length = CFDataGetLength(valueRef);
+  *value = malloc(length);
+  if (*value == NULL) {
+    asprintf(error, "Error allocating memory");
+    return -1;
+  }
+  CFDataGetBytes(valueRef, CFRangeMake(0, length), (unsigned char *)*value);
+  
+  CFRelease(nameRef);
+  CFRelease(valueRef);
+
+  return length;
+}
+
+
+int set(char *name, char *value, int length, char **error)
+{
   CFStringRef   nameRef;
   CFTypeRef     valueRef;
   kern_return_t result;
-  
-  // OF variable name is first.
-  name = str;
-  
-  // Find the equal sign for set
-  while (*str) {
-    if (*str == '=') {
-      set = 1;
-      *str++ = '\0';
-      break;
-    }
-    str++;
-  }
-  
-  if (set == 1) {
-    // On sets, the OF variable's value follows the equal sign.
-    value = str;
-    
-    result = SetOFVariable(name, value);
-    if (result != KERN_SUCCESS) {
-      errx(1, "Error setting variable - '%s': %s", name,
-           mach_error_string(result));
-    }
-  } else {
-    result = GetOFVariable(name, &nameRef, &valueRef);
-    if (result != KERN_SUCCESS) {
-      errx(1, "Error getting variable - '%s': %s", name,
-           mach_error_string(result));
-    }
-    
-    PrintOFVariable(nameRef, valueRef, 0);
-    CFRelease(nameRef);
-    CFRelease(valueRef);
-  }
-}
 
-
-// GetOFVariable(name, nameRef, valueRef)
-//
-//   Get the named firmware variable.
-//   Return it and it's symbol in valueRef and nameRef.
-//
-static kern_return_t GetOFVariable(char *name, CFStringRef *nameRef,
-                                   CFTypeRef *valueRef)
-{
-  *nameRef = CFStringCreateWithCString(kCFAllocatorDefault, name,
-                                       kCFStringEncodingUTF8);
-  if (*nameRef == 0) {
-    errx(1, "Error creating CFString for key %s", name);
-  }
-  
-  *valueRef = IORegistryEntryCreateCFProperty(gOptionsRef, *nameRef, 0, 0);
-  if (*valueRef == 0) return kIOReturnNotFound;
-  
-  return KERN_SUCCESS;
-}
-
-
-// SetOFVariable(name, value)
-//
-//   Set or create an firmware variable with name and value.
-//
-static kern_return_t SetOFVariable(char *name, char *value)
-{
-  CFStringRef   nameRef;
-  CFTypeRef     valueRef;
-   kern_return_t result = KERN_SUCCESS;
-  
   nameRef = CFStringCreateWithCString(kCFAllocatorDefault, name,
                                       kCFStringEncodingUTF8);
   if (nameRef == 0) {
-    errx(1, "Error creating CFString for key %s", name);
+    asprintf(error, "Error creating CFString for key %s", name);
+    return -1;
   }
-  
+
   valueRef = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, (const UInt8 *)value,
-                                           strlen(value), kCFAllocatorDefault);
-  if (valueRef == 0) { }
+                                         length, kCFAllocatorDefault);
+  if (valueRef == 0) {
+    asprintf(error, "Error creating CF buffer");
+    return -1;
+  }
   result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
-  if (result != KERN_SUCCESS) { }
-  
+
   CFRelease(nameRef);
 
-  NVRamSyncNow(name);                     /* Try syncing the new data to device, best effort! */
-  
-  return result;
+  if (result != KERN_SUCCESS) {
+    asprintf(error, "Error setting variable - '%s': %s", name, mach_error_string(result));
+    return -1;
+  }
+
+  return 0;
 }
 
-
-// DeleteOFVariable(name)
-//
-//   Delete the named firmware variable.
-//   
-//
-static void DeleteOFVariable(char *name)
+int Set(char *name, char *value, int length, char **error)
 {
-  SetOFVariable(kIONVRAMDeletePropertyKey, name);
+  int res = set(name, value, length, error);
+  if (res != 0) return res;
+
+  // Try syncing the new data to device, best effort!
+  return set(kIONVRAMSyncNowPropertyKey, name, strlen(name), error);
 }
 
-static void NVRamSyncNow(char *name)
+
+int Delete(char *name, char **error)
 {
-  SetOFVariable(kIONVRAMSyncNowPropertyKey, name);
+  return set(kIONVRAMDeletePropertyKey, name, strlen(name), error);
 }
+
 
 // PrintOFVariables()
 //
 //   Print all of the firmware variables.
 //
-void PrintOFVariables()
-{
-  kern_return_t          result;
-  CFMutableDictionaryRef dict;
-  
-  result = IORegistryEntryCreateCFProperties(gOptionsRef, &dict, 0, 0);
-  if (result != KERN_SUCCESS) {
-    errx(1, "Error getting the firmware variables: %s", mach_error_string(result));
-  }
-
-  CFDictionaryApplyFunction(dict, &PrintOFVariable, 0);
-  
-  CFRelease(dict);
-}
-
-// PrintOFVariable(key, value, context)
+// void PrintOFVariables()
+// {
+//   kern_return_t          result;
+//   CFMutableDictionaryRef dict;
 //
-//   Print the given firmware variable.
+//   result = IORegistryEntryCreateCFProperties(gOptionsRef, &dict, 0, 0);
+//   if (result != KERN_SUCCESS) {
+//     errx(1, "Error getting the firmware variables: %s", mach_error_string(result));
+//   }
 //
-static void PrintOFVariable(const void *key, const void *value, void *context)
-{
-  long          cnt, cnt2;
-  CFIndex       nameLen;
-  char          *nameBuffer = 0;
-  const char    *nameString;
-  const uint8_t *dataPtr;
-  uint8_t       dataChar;
-  char          *dataBuffer = 0;
-  const char    *valueString = 0;
-  uint32_t      length;
-  
-  // Get the OF variable's name.
-  nameLen = CFStringGetLength(key) + 1;
-  nameBuffer = malloc(nameLen);
-  if( nameBuffer && CFStringGetCString(key, nameBuffer, nameLen, kCFStringEncodingUTF8) )
-    nameString = nameBuffer;
-  else {
-    warnx("Unable to convert property name to C string");
-    nameString = "<UNPRINTABLE>";
-  }
-  
-  
-  length = CFDataGetLength(value);
-  if (length == 0) valueString = "";
-  else {
-    dataBuffer = malloc(length * 3 + 1);
-    if (dataBuffer != 0) {
-      dataPtr = CFDataGetBytePtr(value);
-      for (cnt = cnt2 = 0; cnt < length; cnt++) {
-        dataChar = dataPtr[cnt];
-        sprintf(dataBuffer + cnt2, "%%%02x", dataChar);
-        cnt2 += 3;
-      }
-      dataBuffer[cnt2] = '\0';
-      valueString = dataBuffer;
-    }
-  }
-  
-  if ((nameString != 0) && (valueString != 0))
-    printf("%s\t%s\n", nameString, valueString);
-  
-  if (dataBuffer != 0) free(dataBuffer);
-  if (nameBuffer != 0) free(nameBuffer);
-}
+//   CFDictionaryApplyFunction(dict, &PrintOFVariable, 0);
+//
+//   CFRelease(dict);
+// }
--- nvram.go
+++ nvram.go
@@ -1,16 +1,135 @@
 package main
 
 /*
-#cgo CFLAGS: -Wall
 #cgo LDFLAGS: -framework CoreFoundation -framework IOKit
-void setup(void);
-void teardown(void);
-void PrintOFVariables(void);
+
+#import <stdlib.h>
+
+int Setup(char **error);
+void Teardown(void);
+int Get(char *name, char **value, char **error);
+int Set(char *name, char *value, int length, char **error);
+int Delete(char *name, char **error);
 */
 import "C"
 
+import (
+	"errors"
+	"log"
+	"unsafe"
+)
+
 func main() {
-	C.setup()
-	C.PrintOFVariables()
-	C.teardown()
+	err := Setup()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer Teardown()
+
+	res, err := Get("filippo")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Printf("% x\n", res)
+
+	err = Set("filippo", "42è\x00\xff")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Println("Set done")
+
+	res, err = Get("filippo")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Printf("% x\n", res)
+
+	err = Delete("filippo")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Println("Delete done")
+
+	res, err = Get("filippo")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Printf("% x\n", res)
+}
+
+func Setup() error {
+	var errStr *C.char
+	fail := C.Setup(&errStr)
+	if fail != 0 {
+		err := errors.New(C.GoString(errStr))
+		C.free(unsafe.Pointer(errStr))
+		return err
+	}
+	return nil
+}
+
+func Teardown() {
+	C.Teardown()
+}
+
+func Get(name string) (string, error) {
+	nameStr := C.CString(name)
+	defer C.free(unsafe.Pointer(nameStr))
+
+	var value *C.char
+	var errStr *C.char
+	length := C.Get(nameStr, &value, &errStr)
+
+	if length == -1 {
+		err := errors.New(C.GoString(errStr))
+		C.free(unsafe.Pointer(errStr))
+		return "", err
+	} else if length == -2 {
+		return "", nil
+	}
+
+	result := C.GoStringN(value, length)
+
+	C.free(unsafe.Pointer(value))
+
+	return result, nil
+}
+
+func Set(name string, value string) error {
+	nameStr := C.CString(name)
+	defer C.free(unsafe.Pointer(nameStr))
+	valueStr := C.CString(value)
+	defer C.free(unsafe.Pointer(valueStr))
+
+	var errStr *C.char
+	fail := C.Set(nameStr, valueStr, C.int(len(value)), &errStr)
+
+	if fail != 0 {
+		err := errors.New(C.GoString(errStr))
+		C.free(unsafe.Pointer(errStr))
+		return err
+	}
+
+	return nil
+}
+
+func Delete(name string) error {
+	nameStr := C.CString(name)
+	defer C.free(unsafe.Pointer(nameStr))
+
+	var errStr *C.char
+	fail := C.Delete(nameStr, &errStr)
+
+	if fail != 0 {
+		err := errors.New(C.GoString(errStr))
+		C.free(unsafe.Pointer(errStr))
+		return err
+	}
+
+	return nil
 }
