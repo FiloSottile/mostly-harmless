183055647ca82ac5518eecf578b64a0b3c3ee649
Filippo Valsorda <filippo@cloudflare.com>
2014-10-22 15:34:38 +0100

    Remove support for any type except Data

--- nvram.c
+++ nvram.c
@@ -39,7 +39,6 @@ static kern_return_t SetOFVariable(char *name, char *value);
 static void DeleteOFVariable(char *name);
 void PrintOFVariables(void);
 static void PrintOFVariable(const void *key,const void *value,void *context);
-static CFTypeRef ConvertValueToCFTypeRef(CFTypeID typeID, char *value);
 
 static void NVRamSyncNow(char *name);
 
@@ -102,7 +101,6 @@ static void SetOrGetOFVariable(char *str)
     value = str;
     
     result = SetOFVariable(name, value);
-	NVRamSyncNow(name);			/* Try syncing the new data to device, best effort! */
     if (result != KERN_SUCCESS) {
       errx(1, "Error setting variable - '%s': %s", name,
            mach_error_string(result));
@@ -150,7 +148,6 @@ static kern_return_t SetOFVariable(char *name, char *value)
 {
   CFStringRef   nameRef;
   CFTypeRef     valueRef;
-  CFTypeID      typeID;
   kern_return_t result = KERN_SUCCESS;
   
   nameRef = CFStringCreateWithCString(kCFAllocatorDefault, name,
@@ -159,48 +156,15 @@ static kern_return_t SetOFVariable(char *name, char *value)
     errx(1, "Error creating CFString for key %s", name);
   }
   
-  valueRef = IORegistryEntryCreateCFProperty(gOptionsRef, nameRef, 0, 0);
-  if (valueRef) {
-    typeID = CFGetTypeID(valueRef);
-    CFRelease(valueRef);
-    
-    valueRef = ConvertValueToCFTypeRef(typeID, value);
-    if (valueRef == 0) {
-      errx(1, "Error creating CFTypeRef for value %s", value);
-    }  result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
-  } else {
-    while (1) {
-      // In the default case, try data, string, number, then boolean.    
-      
-      valueRef = ConvertValueToCFTypeRef(CFDataGetTypeID(), value);
-      if (valueRef != 0) {
-	result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
-	if (result == KERN_SUCCESS) break;
-      }
-      
-      valueRef = ConvertValueToCFTypeRef(CFStringGetTypeID(), value);
-      if (valueRef != 0) {
-	result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
-	if (result == KERN_SUCCESS) break;
-      }
-      
-      valueRef = ConvertValueToCFTypeRef(CFNumberGetTypeID(), value);
-      if (valueRef != 0) {
-	result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
-	if (result == KERN_SUCCESS) break;
-      }
-      
-      valueRef = ConvertValueToCFTypeRef(CFBooleanGetTypeID(), value);
-      if (valueRef != 0) {
-	result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
-	if (result == KERN_SUCCESS) break;
-      }
-      
-      break;
-    }
-  }
+  valueRef = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, (const UInt8 *)value,
+                                           strlen(value), kCFAllocatorDefault);
+  if (valueRef == 0) { }
+  result = IORegistryEntrySetCFProperty(gOptionsRef, nameRef, valueRef);
+  if (result != KERN_SUCCESS) { }
   
   CFRelease(nameRef);
+
+  NVRamSyncNow(name);                     /* Try syncing the new data to device, best effort! */
   
   return result;
 }
@@ -250,15 +214,11 @@ static void PrintOFVariable(const void *key, const void *value, void *context)
   CFIndex       nameLen;
   char          *nameBuffer = 0;
   const char    *nameString;
-  char          numberBuffer[10];
   const uint8_t *dataPtr;
   uint8_t       dataChar;
   char          *dataBuffer = 0;
-  CFIndex       valueLen;
-  char          *valueBuffer = 0;
   const char    *valueString = 0;
-  uint32_t      number, length;
-  CFTypeID      typeID;
+  uint32_t      length;
   
   // Get the OF variable's name.
   nameLen = CFStringGetLength(key) + 1;
@@ -270,48 +230,21 @@ static void PrintOFVariable(const void *key, const void *value, void *context)
     nameString = "<UNPRINTABLE>";
   }
   
-  // Get the OF variable's type.
-  typeID = CFGetTypeID(value);
   
-  if (typeID == CFBooleanGetTypeID()) {
-    if (CFBooleanGetValue(value)) valueString = "true";
-    else valueString = "false";
-  } else if (typeID == CFNumberGetTypeID()) {
-    CFNumberGetValue(value, kCFNumberSInt32Type, &number);
-    if (number == 0xFFFFFFFF) sprintf(numberBuffer, "-1");
-    else if (number < 1000) sprintf(numberBuffer, "%d", number);
-    else sprintf(numberBuffer, "0x%x", number);
-    valueString = numberBuffer;
-  } else if (typeID == CFStringGetTypeID()) {
-    valueLen = CFStringGetLength(value) + 1;
-    valueBuffer = malloc(valueLen + 1);
-    if ( valueBuffer && CFStringGetCString(value, valueBuffer, valueLen, kCFStringEncodingUTF8) )
-      valueString = valueBuffer;
-    else {
-      warnx("Unable to convert value to C string");
-      valueString = "<UNPRINTABLE>";
-    }
-  } else if (typeID == CFDataGetTypeID()) {
-    length = CFDataGetLength(value);
-    if (length == 0) valueString = "";
-    else {
-      dataBuffer = malloc(length * 3 + 1);
-      if (dataBuffer != 0) {
-	dataPtr = CFDataGetBytePtr(value);
-	for (cnt = cnt2 = 0; cnt < length; cnt++) {
-	  dataChar = dataPtr[cnt];
-	  if (isprint(dataChar)) dataBuffer[cnt2++] = dataChar;
-	  else {
-	    sprintf(dataBuffer + cnt2, "%%%02x", dataChar);
-	    cnt2 += 3;
-	  }
-	}
-	dataBuffer[cnt2] = '\0';
-	valueString = dataBuffer;
+  length = CFDataGetLength(value);
+  if (length == 0) valueString = "";
+  else {
+    dataBuffer = malloc(length * 3 + 1);
+    if (dataBuffer != 0) {
+      dataPtr = CFDataGetBytePtr(value);
+      for (cnt = cnt2 = 0; cnt < length; cnt++) {
+        dataChar = dataPtr[cnt];
+        sprintf(dataBuffer + cnt2, "%%%02x", dataChar);
+        cnt2 += 3;
       }
+      dataBuffer[cnt2] = '\0';
+      valueString = dataBuffer;
     }
-  } else {
-    valueString="<INVALID>";
   }
   
   if ((nameString != 0) && (valueString != 0))
@@ -319,47 +252,4 @@ static void PrintOFVariable(const void *key, const void *value, void *context)
   
   if (dataBuffer != 0) free(dataBuffer);
   if (nameBuffer != 0) free(nameBuffer);
-  if (valueBuffer != 0) free(valueBuffer);
-}
-
-
-// ConvertValueToCFTypeRef(typeID, value)
-//
-//   Convert the value into a CFType given the typeID.
-//
-static CFTypeRef ConvertValueToCFTypeRef(CFTypeID typeID, char *value)
-{
-  CFTypeRef     valueRef = 0;
-  long          cnt, cnt2, length;
-  unsigned long number, tmp;
-  
-  if (typeID == CFBooleanGetTypeID()) {
-    if (!strcmp("true", value)) valueRef = kCFBooleanTrue;
-    else if (!strcmp("false", value)) valueRef = kCFBooleanFalse;
-  } else if (typeID == CFNumberGetTypeID()) {
-    number = strtol(value, 0, 0);
-    valueRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type,
-			      &number);
-  } else if (typeID == CFStringGetTypeID()) {
-    valueRef = CFStringCreateWithCString(kCFAllocatorDefault, value,
-					 kCFStringEncodingUTF8);
-  } else if (typeID == CFDataGetTypeID()) {
-    length = strlen(value);
-    for (cnt = cnt2 = 0; cnt < length; cnt++, cnt2++) {
-      if (value[cnt] == '%') {
-	if (!ishexnumber(value[cnt + 1]) ||
-	    !ishexnumber(value[cnt + 2])) return 0;
-	number = toupper(value[++cnt]) - '0';
-	if (number > 9) number -= 7;
-	tmp = toupper(value[++cnt]) - '0';
-	if (tmp > 9) tmp -= 7;
-	number = (number << 4) + tmp;
-	value[cnt2] = number;
-      } else value[cnt2] = value[cnt];
-    }
-    valueRef = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, (const UInt8 *)value,
-					   cnt2, kCFAllocatorDefault);
-  } else return 0;
-  
-  return valueRef;
 }
