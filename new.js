14fa2c20498251c6b732c9a1590e5aff7fe517f6
Filippo Valsorda <filippo@cloudflare.com>
2015-02-21 05:53:40 +0000

    finished new

--- new.js
+++
@@ -1,5 +0,0 @@
-if (typeof BADFISH_SAN !== 'undefined') document.getElementById("result").innerHTML += "san "
-if (typeof BADFISH_SELF !== 'undefined') document.getElementById("result").innerHTML += "self "
-if (typeof BADFISH_SUPER !== 'undefined') document.getElementById("result").innerHTML += "super "
-
-if (typeof console !== 'undefined') console.log('Loaded new');
