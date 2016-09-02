524c7be3233fa2aa4fdd64e2ff818f66327dd047
Filippo Valsorda <filippo@cloudflare.com>
2016-09-02 17:33:48 +0100

    Initial commit with 1.3/1.2 handshake support

---
+++ histogram.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+	"time"
+)
+
+const (
+	barChar = "∎"
+)
+
+type histogram struct {
+	points  []time.Duration
+	slowest time.Duration
+	fastest time.Duration
+}
+
+// newHistogram produces an empty histogram.
+func newHistogram() *histogram {
+	return &histogram{
+		slowest: 0,
+		fastest: time.Duration(math.MaxInt64),
+	}
+}
+
+// Observe updates the histogram with a new measurement.
+func (h *histogram) Observe(d time.Duration) {
+	h.points = append(h.points, d)
+	if d > h.slowest {
+		h.slowest = d
+	}
+	if d < h.fastest {
+		h.fastest = d
+	}
+}
+
+type ByDuration []time.Duration
+
+func (a ByDuration) Len() int           { return len(a) }
+func (a ByDuration) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByDuration) Less(i, j int) bool { return a[i] < a[j] }
+
+// Print formats the histogram data and prints it to stdout.
+func (h *histogram) Print(log bool) {
+	sort.Sort(ByDuration(h.points))
+
+	// buckets is the upper threshold for each bucket in the hist
+	buckets := make([]time.Duration, 8)
+	if log {
+		bs := h.slowest - h.fastest
+		for i := range buckets {
+			buckets[len(buckets)-1-i] = h.fastest + bs
+			bs = bs / 2
+		}
+	} else {
+		bs := int64(h.slowest-h.fastest)/int64(len(buckets)-1) + 1
+		for i := range buckets {
+			buckets[i] = h.fastest + time.Duration(bs*int64(i))
+		}
+	}
+
+	// counts is the number of latencies that fell in each bucket.
+	counts := make([]int, len(buckets))
+	var bi, max int
+	for i := 0; i < len(h.points); {
+		if h.points[i] <= buckets[bi] {
+			i++
+			counts[bi]++
+			if max < counts[bi] {
+				max = counts[bi]
+			}
+		} else if bi < len(buckets)-1 {
+			bi++
+		} else {
+			panic(fmt.Sprintf("%d is higher than %d", h.points[i], buckets[bi]))
+		}
+	}
+
+	// Print histogram to stdout.
+	fmt.Printf("Response time histogram:\n")
+	lowerBound := time.Duration(0)
+	for i, upperBound := range buckets {
+		// Normalize bar lengths.
+		var barLen int
+		if max > 0 {
+			barLen = counts[i] * 40 / max
+		}
+		fmt.Printf("  %3dms - %3dms [%v]\t|%v\n", lowerBound/time.Millisecond,
+			upperBound/time.Millisecond, counts[i], strings.Repeat(barChar, barLen))
+		lowerBound = upperBound
+	}
+}
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"runtime"
+	"sync"
+	"time"
+
+	tls "github.com/google/boringssl/ssl/test/runner"
+	"gopkg.in/cheggaaa/pb.v1"
+)
+
+type Config struct {
+	Targets []struct {
+		Name    string
+		Address string // host:port
+	}
+	SNI            string
+	Version        string // tls12, tls13
+	AllowDowngrade bool
+	Parallel       int // If 0, matches number of CPUs
+	Timeout        string
+	Repeats        int
+}
+
+type job struct {
+	Name    string
+	Address string
+	Timeout time.Duration
+
+	h         *histogram
+	bar       *pb.ProgressBar
+	tlsConfig *tls.Config
+}
+
+var outputMu sync.Mutex
+
+func runJob(j *job) {
+	start := time.Now()
+	conn, err := tls.DialWithDialer(&net.Dialer{
+		Timeout: j.Timeout,
+	}, "tcp", j.Address, j.tlsConfig)
+	outputMu.Lock()
+	j.bar.Increment()
+	j.bar.Update()
+	if err != nil {
+		fmt.Printf("\r\033[K\x1b\x5b\x31\x6d%v\x1b\x5b\x30\x6d: %v (%v)\n", j.Name, err, time.Since(start))
+		fmt.Print(j.bar.String())
+		outputMu.Unlock()
+	} else {
+		j.h.Observe(time.Since(start))
+		outputMu.Unlock()
+		conn.Close()
+	}
+}
+
+func main() {
+	c := &Config{}
+	if err := json.NewDecoder(os.Stdin).Decode(c); err != nil {
+		log.Fatal(err)
+	}
+
+	timeout, err := time.ParseDuration(c.Timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var version uint16
+	if c.Version == "tls13" {
+		version = tls.VersionTLS13
+	} else if c.Version == "tls12" {
+		version = tls.VersionTLS12
+	} else {
+		log.Fatal("Invalid Version field")
+	}
+	tlsConfig := &tls.Config{
+		MaxVersion: version,
+		ServerName: c.SNI,
+	}
+	if !c.AllowDowngrade {
+		tlsConfig.MinVersion = version
+	}
+
+	h := newHistogram()
+	bar := pb.New(len(c.Targets) * c.Repeats)
+	bar.ShowTimeLeft = false
+	bar.ManualUpdate = true
+	bar.Start()
+
+	jobChan := make(chan *job)
+
+	workers := c.Parallel
+	if workers == 0 {
+		workers = runtime.GOMAXPROCS(-1)
+	}
+	var wg sync.WaitGroup
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			for j := range jobChan {
+				runJob(j)
+			}
+			wg.Done()
+		}()
+	}
+
+	fmt.Print("\n")
+
+	for n := 0; n < c.Repeats; n++ {
+		for _, t := range c.Targets {
+			jobChan <- &job{
+				Name:      t.Name,
+				Address:   t.Address,
+				Timeout:   timeout,
+				tlsConfig: tlsConfig,
+				h:         h,
+				bar:       bar,
+			}
+		}
+	}
+	close(jobChan)
+	wg.Wait()
+
+	bar.Finish()
+	fmt.Print("\n")
+	h.Print(true)
+	fmt.Printf("\nFastest: %s - Slowest: %s\n\n", h.fastest, h.slowest)
+}
