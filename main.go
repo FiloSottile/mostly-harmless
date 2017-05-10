89fa24b63e56e58d563845b7ce3af3a052ec3ce3
Filippo Valsorda <filippo@cloudflare.com>
2017-05-10 11:46:06 -0400

    UI

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
@@ -17,12 +20,51 @@ var state = make(map[string]entry)
 
 func main() {
 	serv := http.Server{
-		Addr: ":8080",
+		Addr: os.Args[1],
 	}
 
 	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/stats", stats)
+	http.HandleFunc("/speaker", serveHTML("speaker.html"))
+	http.HandleFunc("/", serveHTML("index.html"))
 
 	log.Fatal(serv.ListenAndServe())
+}
+
+func serveHTML(filename string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		contents, err := ioutil.ReadFile(filename)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.Write(contents)
+	}
+}
+
+func stats(w http.ResponseWriter, r *http.Request) {
+	stateMu.Lock()
+	defer stateMu.Unlock()
+
+	gotit, confused := 0, 0
+	for user := range state {
+		if state[user].timestamp.Before(time.Now().Add(-time.Minute)) {
+			continue
+		}
+		if state[user].type_ == "gotit" {
+			gotit++
+		} else {
+			confused++
+		}
+	}
+
+	json.NewEncoder(w).Encode(struct {
+		Gotit    int `json:"gotit"`
+		Confused int `json:"confused"`
+	}{
+		Gotit:    gotit,
+		Confused: confused,
+	})
 }
 
 func submit(w http.ResponseWriter, r *http.Request) {
@@ -41,7 +83,7 @@ func submit(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "NO USER ID OH NO", 400)
 		return
 	}
-	log.Printf("received vote %s from %s", type_, user)
+	log.Printf("%s -> %s", user, type_)
 	stateMu.Lock()
 	defer stateMu.Unlock()
 	state[user] = entry{
