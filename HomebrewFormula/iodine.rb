0a05618b81697181f9ac8e5af791d08dd99c9499
Filippo Valsorda <hi@filippo.io>
2017-10-30 16:55:26 -0400

    Add HomebrewFormula

---
+++ HomebrewFormula/iodine.rb
@@ -0,0 +1,30 @@
+class Iodine < Formula
+  desc "Tool for tunneling IPv4 data through a DNS server"
+  homepage "http://code.kryo.se/iodine/"
+  head "https://github.com/yarrick/iodine.git"
+
+  stable do
+    url "http://code.kryo.se/iodine/iodine-0.7.0.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iodine/iodine_0.7.0.orig.tar.gz"
+    sha256 "ad2b40acf1421316ec15800dcde0f587ab31d7d6f891fa8b9967c4ded93c013e"
+
+    depends_on :tuntap
+  end
+
+  # Requires tuntap for < 10.10
+  depends_on :macos => :yosemite
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  def caveats; <<-EOS.undent
+      To use the native utun devices instead of tuntap, install
+      the HEAD version, and use "-d utunX"
+    EOS
+  end
+
+  test do
+    system "#{sbin}/iodine", "-v"
+  end
+end
