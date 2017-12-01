1e6b633a954fe11c763fe4f75e3f6666de3d8fe2
Filippo Valsorda <hi@filippo.io>
2017-12-01 06:10:34 +0000

    Add stubby Formula with fix for getdnsapi/stubby#48 and less logging

---
+++ HomebrewFormula/stubby.rb
@@ -0,0 +1,83 @@
+class Stubby < Formula
+  desc "DNS privacy enabled stub resolver service based on getdns"
+  homepage "https://getdnsapi.net/blog/dns-privacy-daemon-stubby/"
+  url "https://github.com/getdnsapi/stubby/archive/v0.1.5.tar.gz"
+  sha256 "f909cd56922e861c830ad6fe3c6f18e1100704345f17891368df9f9430aef80c"
+  head "https://github.com/getdnsapi/stubby.git", :branch => "develop"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "getdns"
+  depends_on "libyaml"
+
+  def install
+    inreplace "src/stubby.c", "if (msg->cd_bit)", "if (0)" # https://github.com/getdnsapi/stubby/issues/48
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          # "CFLAGS=-DSERVER_DEBUG=1",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
+    system "make", "install"
+  end
+
+  plist_options :startup => true, :manual => "sudo stubby -C #{HOMEBREW_PREFIX}/etc/stubby/stubby.yml"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-/Apple/DTD PLIST 1.0/EN" "http:/www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/stubby</string>
+          <string>-C</string>
+          <string>#{etc}/stubby/stubby.yml</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/stubby/stubby.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/stubby/stubby.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    assert_predicate etc/"stubby/stubby.yml", :exist?
+    (testpath/"stubby_test.yml").write <<~EOS
+      resolution_type: GETDNS_RESOLUTION_STUB
+      dns_transport_list:
+        - GETDNS_TRANSPORT_TLS
+        - GETDNS_TRANSPORT_UDP
+        - GETDNS_TRANSPORT_TCP
+      listen_addresses:
+        - 127.0.0.1@5553
+      idle_timeout: 0
+      upstream_recursive_servers:
+        - address_data: 145.100.185.15
+        - address_data: 145.100.185.16
+        - address_data: 185.49.141.37
+    EOS
+    output = shell_output("#{bin}/stubby -i -C stubby_test.yml")
+    assert_match "bindata for 145.100.185.15", output
+    pid = fork do
+      exec "#{bin}/stubby", "-C", testpath/"stubby_test.yml"
+    end
+    begin
+      sleep 2
+      output = shell_output("dig @127.0.0.1 -p 5553 getdnsapi.net")
+      assert_match "status: NOERROR", output
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
+  end
+end
