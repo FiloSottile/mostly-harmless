86348435de2539a1eeb9bafa463b9987a6334996
Filippo Valsorda <hi@filippo.io>
2018-02-19 12:58:15 +0100

    covfefe: refactor spaghetti

--- covfefe/main.go
+++ covfefe/main.go
@@ -38,72 +38,17 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	mustExec := func(query string, args ...interface{}) {
-		_, err = db.Exec(query, args...)
-		if err != nil {
-			debug.PrintStack()
-			log.Fatalln("Failed to execute query:", err)
-		}
-	}
-	mustInsert := func(query string, args ...interface{}) (id int64) {
-		res, err := db.Exec(query, args...)
-		if err != nil {
-			debug.PrintStack()
-			log.Fatalln("Failed to execute insert:", err)
-		}
-		id, err = res.LastInsertId()
-		if err != nil {
-			debug.PrintStack()
-			log.Fatalln("Failed to get id:", err)
-		}
-		return id
-	}
 
-	mustExec(`CREATE TABLE IF NOT EXISTS Messages (
-		id INTEGER PRIMARY KEY,
-		created TEXT NOT NULL,
-		json TEXT NOT NULL
-	)`)
-	insertMessage := func(object interface{}, created time.Time) (id int64) {
-		return mustInsert(
-			`INSERT INTO Messages (created, json) VALUES (?, ?)`,
-			created, mustMarshal(object))
+	c := &Covfefe{
+		db: db,
+		httpClient: &http.Client{
+			Timeout: 1 * time.Minute,
+		},
 	}
-	mustExec(`CREATE TABLE IF NOT EXISTS Tweets (
-		id INTEGER PRIMARY KEY,
-		created TEXT NOT NULL,
-		user TEXT NOT NULL,
-		message INTEGER NOT NULL REFERENCES Messages(id),
-		deleted TEXT
-	)`)
-	insertTweet := func(tweet *twitter.Tweet, message int64) (new bool) {
-		_, err := db.Exec(
-			`INSERT INTO Tweets (id, created, user, message) VALUES (?, ?, ?, ?)`,
-			tweet.ID, mustTimeParse(tweet.CreatedAt), tweet.User.ScreenName, message)
-		if err, ok := err.(sqlite3.Error); ok && err.ExtendedCode != sqlite3.ErrConstraintUnique {
-			return false
-		}
-		if err != nil {
-			debug.PrintStack()
-			log.Fatalln("Failed to execute insert:", err)
-		}
-		return true
-	}
-	deletedTweet := func(id int64) {
-		mustExec(`UPDATE Tweets SET deleted = datetime('now') WHERE id = ?`, id)
-	}
-	mustExec(`CREATE TABLE IF NOT EXISTS Media (
-		id INTEGER PRIMARY KEY,
-		media BLOB NOT NULL,
-		tweet INTEGER NOT NULL REFERENCES Tweets(id)
-	)`)
-	insertMedia := func(data []byte, id, tweet int64) {
-		mustInsert(`INSERT INTO Media (id, media, tweet) VALUES (?, ?, ?)`, id, data, tweet)
-	}
+	c.initDB()
 
 	config := oauth1.NewConfig(creds["API_KEY"], creds["API_SECRET"])
-	// TODO: multiple tokens
-	token := oauth1.NewToken(creds["TOKEN"], creds["TOKEN_SECRET"])
+	token := oauth1.NewToken(creds["TOKEN"], creds["TOKEN_SECRET"]) // TODO: multiple tokens
 	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
 
@@ -117,63 +62,15 @@ func main() {
 	}
 	demux := twitter.NewSwitchDemux()
 
-	var wg sync.WaitGroup
-
 	demux.Tweet = func(tweet *twitter.Tweet) {
-		messageID := insertMessage(tweet, mustTimeParse(tweet.CreatedAt))
-
-		var processTweet func(*twitter.Tweet)
-		processTweet = func(tweet *twitter.Tweet) {
-			if tweet.User.Protected {
-				return
-			}
-			if insertTweet(tweet, messageID) {
-				var media []twitter.MediaEntity
-				if tweet.Entities != nil {
-					media = tweet.Entities.Media
-				}
-				if tweet.ExtendedEntities != nil {
-					media = tweet.ExtendedEntities.Media
-				}
-				if tweet.ExtendedTweet != nil {
-					if tweet.ExtendedTweet.Entities != nil {
-						media = tweet.ExtendedTweet.Entities.Media
-					}
-					if tweet.ExtendedTweet.ExtendedEntities != nil {
-						media = tweet.ExtendedTweet.ExtendedEntities.Media
-					}
-				}
-				if len(media) != 0 {
-					wg.Add(1)
-					go func() {
-						for _, m := range media {
-							if m.SourceStatusID != 0 {
-								// We'll find this media attached to the retweet.
-								continue
-							}
-							insertMedia(mustGet(m.MediaURLHttps), m.ID, tweet.ID)
-							// TODO: archive videos?
-						}
-						wg.Done()
-					}()
-				}
-				if tweet.RetweetedStatus != nil {
-					processTweet(tweet.RetweetedStatus)
-				}
-				if tweet.QuotedStatus != nil {
-					processTweet(tweet.QuotedStatus)
-				}
-				// TODO: crawl thread, non-embedded linked tweets
-			}
-		}
-
-		processTweet(tweet)
+		messageID := c.insertMessage(tweet, mustTimeParse(tweet.CreatedAt))
+		c.processTweet(messageID, tweet)
 	}
 	demux.StatusDeletion = func(deletion *twitter.StatusDeletion) {
-		deletedTweet(deletion.ID)
+		c.deletedTweet(deletion.ID)
 	}
 	demux.Event = func(event *twitter.Event) {
-		insertMessage(event, mustTimeParse(event.CreatedAt))
+		c.insertMessage(event, mustTimeParse(event.CreatedAt))
 	}
 
 	demux.StreamLimit = func(limit *twitter.StreamLimit) {
@@ -198,7 +95,130 @@ func main() {
 	log.Println(<-ch)
 
 	stream.Stop()
-	wg.Wait()
+	c.wg.Wait()
+}
+
+type Covfefe struct {
+	db         *sql.DB
+	wg         sync.WaitGroup
+	httpClient *http.Client
+}
+
+func (c *Covfefe) mustExec(query string, args ...interface{}) {
+	_, err := c.db.Exec(query, args...)
+	if err != nil {
+		debug.PrintStack()
+		log.Fatalln("Failed to execute query:", err)
+	}
+}
+
+func (c *Covfefe) mustInsert(query string, args ...interface{}) (id int64) {
+	res, err := c.db.Exec(query, args...)
+	if err != nil {
+		debug.PrintStack()
+		log.Fatalln("Failed to execute insert:", err)
+	}
+	id, err = res.LastInsertId()
+	if err != nil {
+		debug.PrintStack()
+		log.Fatalln("Failed to get id:", err)
+	}
+	return id
+}
+
+func (c *Covfefe) initDB() {
+	c.mustExec(`CREATE TABLE IF NOT EXISTS Messages (
+		id INTEGER PRIMARY KEY,
+		created TEXT NOT NULL,
+		json TEXT NOT NULL
+	)`)
+	c.mustExec(`CREATE TABLE IF NOT EXISTS Tweets (
+		id INTEGER PRIMARY KEY,
+		created TEXT NOT NULL,
+		user TEXT NOT NULL,
+		message INTEGER NOT NULL REFERENCES Messages(id),
+		deleted TEXT
+	)`)
+	c.mustExec(`CREATE TABLE IF NOT EXISTS Media (
+		id INTEGER PRIMARY KEY,
+		media BLOB NOT NULL,
+		tweet INTEGER NOT NULL REFERENCES Tweets(id)
+	)`)
+}
+
+func (c *Covfefe) insertMessage(object interface{}, created time.Time) (id int64) {
+	return c.mustInsert(
+		`INSERT INTO Messages (created, json) VALUES (?, ?)`,
+		created, mustMarshal(object))
+}
+
+func (c *Covfefe) insertTweet(tweet *twitter.Tweet, message int64) (new bool) {
+	_, err := c.db.Exec(
+		`INSERT INTO Tweets (id, created, user, message) VALUES (?, ?, ?, ?)`,
+		tweet.ID, mustTimeParse(tweet.CreatedAt), tweet.User.ScreenName, message)
+	if err, ok := err.(sqlite3.Error); ok && err.ExtendedCode != sqlite3.ErrConstraintUnique {
+		return false
+	}
+	if err != nil {
+		debug.PrintStack()
+		log.Fatalln("Failed to execute insert:", err)
+	}
+	return true
+}
+
+func (c *Covfefe) insertMedia(data []byte, id, tweet int64) {
+	c.mustInsert(`INSERT INTO Media (id, media, tweet) VALUES (?, ?, ?)`, id, data, tweet)
+}
+
+func (c *Covfefe) deletedTweet(id int64) {
+	c.mustExec(`UPDATE Tweets SET deleted = datetime('now') WHERE id = ?`, id)
+}
+
+func (c *Covfefe) processTweet(messageID int64, tweet *twitter.Tweet) {
+	if tweet.User.Protected {
+		return
+	}
+	if !c.insertTweet(tweet, messageID) {
+		return
+	}
+
+	var media []twitter.MediaEntity
+	if tweet.Entities != nil {
+		media = tweet.Entities.Media
+	}
+	if tweet.ExtendedEntities != nil {
+		media = tweet.ExtendedEntities.Media
+	}
+	if tweet.ExtendedTweet != nil {
+		if tweet.ExtendedTweet.Entities != nil {
+			media = tweet.ExtendedTweet.Entities.Media
+		}
+		if tweet.ExtendedTweet.ExtendedEntities != nil {
+			media = tweet.ExtendedTweet.ExtendedEntities.Media
+		}
+	}
+	if len(media) != 0 {
+		c.wg.Add(1)
+		go func() {
+			for _, m := range media {
+				if m.SourceStatusID != 0 {
+					// We'll find this media attached to the retweet.
+					continue
+				}
+				c.insertMedia(c.mustGet(m.MediaURLHttps), m.ID, tweet.ID)
+				// TODO: archive videos?
+			}
+			c.wg.Done()
+		}()
+	}
+
+	if tweet.RetweetedStatus != nil {
+		c.processTweet(messageID, tweet.RetweetedStatus)
+	}
+	if tweet.QuotedStatus != nil {
+		c.processTweet(messageID, tweet.QuotedStatus)
+	}
+	// TODO: crawl thread, non-embedded linked tweets
 }
 
 func mustMarshal(v interface{}) []byte {
@@ -217,12 +237,8 @@ func mustTimeParse(CreatedAt string) time.Time {
 	return t
 }
 
-var httpClient = &http.Client{
-	Timeout: 1 * time.Minute,
-}
-
-func mustGet(url string) []byte {
-	res, err := httpClient.Get(url)
+func (c *Covfefe) mustGet(url string) []byte {
+	res, err := c.httpClient.Get(url)
 	if err != nil {
 		log.Fatalln("Failed HTTP request:", err)
 	}
