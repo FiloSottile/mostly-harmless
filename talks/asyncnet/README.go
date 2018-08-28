130b607ba73b828c277b920d8e03621b01d4fb83
Filippo Valsorda <hi@filippo.io>
2018-08-28 13:33:56 -0600

    Update README.go

--- talks/asyncnet/README.go
+++ talks/asyncnet/README.go
@@ -1,4 +1,4 @@
 This is the code for my GopherCon 2018 talk "Asynchronous Networking",
 an introduction to the net package, livecoding a TLS-aware TCP proxy.
 
-Slides: https://speakerdeck.com/filosottile/asynchronous-networking-at-gophercon-2018
+Slides → https://speakerdeck.com/filosottile/asynchronous-networking-at-gophercon-2018
