22c6a9e08ad95b602b470652ad2d401d8750e264
Filippo Valsorda <hi@filippo.io>
2020-02-03 14:37:52 -0800

    rare: add metrics

--- rare/go.mod
+++ rare/go.mod
@@ -1,3 +1,8 @@
 module github.com/FiloSottile/mostly-harmless/rare
 
 go 1.13
+
+require (
+	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/influxdata/influxdb v1.7.9
+)
--- rare/main.go
+++ rare/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/xml"
 	"errors"
+	"expvar"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -18,18 +19,35 @@ import (
 func main() {
 	bookmark, err := ioutil.ReadFile("bookmark")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to read bookmark: %v", err)
 	}
-	first, err := time.Parse("2006-01-02",
+	next, err := time.Parse("2006-01-02",
 		strings.TrimSpace(string(bookmark)))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse bookmark: %v", err)
 	}
+	startInfluxDB()
+	expvar.Publish("next", expvar.Func(func() interface{} {
+		return next.Format("2006-01-02")
+	}))
 	r := &Rare{
-		hc:   &http.Client{Timeout: 10 * time.Second},
-		rate: time.NewTicker(time.Second),
+		hc:      &http.Client{Timeout: 10 * time.Second},
+		rate:    time.NewTicker(time.Second),
+		fetches: expvar.NewInt("fetches"),
+		errors:  expvar.NewInt("errors"),
 	}
-	r.fetchSitemaps(first)
+	for {
+		time.Sleep(time.Until(next.Add(36 * time.Hour)))
+		if err := r.fetchSitemap(next); err != nil {
+			log.Printf("Error fetching sitemap %s: %v", next.Format("2006-01-02"), err)
+			r.errors.Add(1)
+			continue
+		}
+		next = next.AddDate(0, 0, 1)
+		if err := ioutil.WriteFile("bookmark", []byte(next.Format("2006-01-02")), 0664); err != nil {
+			log.Fatalf("Failed to write bookmark: %v", err)
+		}
+	}
 }
 
 const postsSitemap = "https://medium.com/sitemap/posts/%s/posts-%s.xml"
@@ -37,54 +55,48 @@ const postsSitemap = "https://medium.com/sitemap/posts/%s/posts-%s.xml"
 type Rare struct {
 	hc   *http.Client
 	rate *time.Ticker
+
+	fetches *expvar.Int
+	errors  *expvar.Int
 }
 
-func (r *Rare) fetchSitemaps(next time.Time) {
-	for {
-		if next.After(time.Now()) {
-			return
-		}
-		r.fetchSitemap(next)
-		next = next.AddDate(0, 0, 1)
-		if err := ioutil.WriteFile("bookmark", []byte(next.Format("2006-01-02")), 0664); err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func (r *Rare) fetchSitemap(t time.Time) {
+func (r *Rare) fetchSitemap(t time.Time) error {
 	date := t.Format("2006-01-02")
 	log.Printf("Fetching %s sitemap...", date)
 	<-r.rate.C
 	resp, err := r.hc.Get(fmt.Sprintf(postsSitemap, t.Format("2006"), date))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	buf := &bytes.Buffer{}
 	f, err := createFile(fmt.Sprintf("sitemap/posts-%s.xml", date))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	if _, err := io.Copy(io.MultiWriter(buf, f), resp.Body); err != nil {
-		log.Fatal(err)
+		return err
 	}
 	resp.Body.Close()
 	if err := f.Close(); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	var sitemap struct {
 		URLs []string `xml:"url>loc"`
 	}
 	if err := xml.Unmarshal(buf.Bytes(), &sitemap); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	for _, url := range sitemap.URLs {
 		if err := r.fetchPost(url); err != nil {
-			log.Printf("error fetching post %q: %v", url, err)
+			log.Printf("Error fetching post %q: %v", url, err)
+			r.errors.Add(1)
+			continue
 		}
+		r.fetches.Add(1)
 	}
+	return nil
 }
 
 func (r *Rare) fetchPost(url string) error {
