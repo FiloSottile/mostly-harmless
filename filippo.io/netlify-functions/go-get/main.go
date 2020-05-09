6a378b8c218543e0d3f914a57acc31cd0f198847
Filippo Valsorda <hi@filippo.io>
2020-05-09 18:57:37 -0400

    try

--- filippo.io/netlify-functions/go-get/main.go
+++ filippo.io/netlify-functions/go-get/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	"encoding/json"
+	"go/build"
+	"html/template"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var templateHTML = template.Must(template.New("go-get.html").Parse(`
+<body>
+    <p>GOPATH is {{.GOPATH}} 
+    <p>path is {{.PATH}} 
+`))
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	res, _ := json.MarshalIndent(request, "", "\t")
+	path := strings.TrimPrefix(request.Path, "/.netlify/functions/go-get")
+	buf := &strings.Builder{}
+	templateHTML.Execute(buf, map[string]string{
+		"GOPATH": build.Default.GOPATH,
+		"PATH":   path,
+	})
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       string(res),
+		Headers: map[string]string{
+			"Content-Type": "text/html; charset=UTF-8",
+		},
+		Body: buf.String(),
 	}, nil
 }
 
