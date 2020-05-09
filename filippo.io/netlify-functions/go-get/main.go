6e19c378051aad3e3d61be1ebb85b9438be83f2d
Filippo Valsorda <hi@filippo.io>
2020-05-09 18:46:57 -0400

    debug

--- filippo.io/netlify-functions/go-get/main.go
+++ filippo.io/netlify-functions/go-get/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	res, _ := json.MarshalIndent(request, "", "\t")
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World",
+		Body:       string(res),
 	}, nil
 }
 
