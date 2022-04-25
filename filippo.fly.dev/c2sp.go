f6977401567f92be720104dd73c33e314caf9d1d
Filippo Valsorda <hi@filippo.io>
2022-04-25 13:30:06 +0200

    filippo.fly.dev: regex-based c2sp.org redirect

--- filippo.fly.dev/c2sp.go
+++ filippo.fly.dev/c2sp.go
@@ -1,11 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
 func c2sp(mux *http.ServeMux) {
-	handleWithCounter(mux, "c2sp.org/", http.RedirectHandler(
-		"https://github.com/C2SP/C2SP", http.StatusFound))
+	specRe := regexp.MustCompile(`^/[a-z0-9-]+$`)
 
-	handleWithCounter(mux, "c2sp.org/age", http.RedirectHandler(
-		"https://github.com/C2SP/C2SP/blob/main/age.md", http.StatusFound))
+	handleFuncWithCounter(mux, "c2sp.org/", func(w http.ResponseWriter, r *http.Request) {
+		if path := r.URL.Path; specRe.MatchString(path) {
+			http.Redirect(w, r, "https://github.com/C2SP/C2SP/blob/main"+path+".md", http.StatusFound)
+			return
+		}
+		http.Redirect(w, r, "https://github.com/C2SP/C2SP", http.StatusFound)
+	})
 }
