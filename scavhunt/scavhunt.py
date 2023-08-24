233efab969791a3f3e7301888bf057edf3edba4c
Filippo Valsorda <hi@filippo.io>
2023-08-24 09:11:24 +0200

    scavhunt: poll delivery status

--- scavhunt/scavhunt.py
+++ scavhunt/scavhunt.py
@@ -7,18 +7,35 @@ msg = "What's with the nails? Item 84. This is text message %d of 5840. The next
 last = datetime.fromisoformat("2024-08-12T00:00:00")
 now = datetime.now().replace(microsecond=0)
 
-if now >= last: sys.exit(42)
+if now >= last:
+    sys.exit(42)
 count = math.floor(5840 - (last - now) / timedelta(hours=3)) + 1
 m = msg % (count, (now + timedelta(hours=3)).isoformat())
 print(m)
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
-from_ = os.environ['SCAVHUNT_FROM']
-to = os.environ['SCAVHUNT_TO']
-message = client.messages.create(body = m, from_=from_, to=to)
+from_ = os.environ["SCAVHUNT_FROM"]
+to = os.environ["SCAVHUNT_TO"]
+message = client.messages.create(body=m, from_=from_, to=to)
 
-hc_uuid = os.environ['SCAVHUNT_HC_UUID']
-requests.post("https://hc-ping.com/" + hc_uuid, data="sid=" + message.sid, timeout=10)
+tries = 0
+while message.status not in ("delivered", "failed", "undelivered") and tries < 60:
+    message = client.messages(message.sid).fetch()
+    print(message.status)
+    time.sleep(1)
+    tries += 1
+
+hc_uuid = os.environ["SCAVHUNT_HC_UUID"]
+if message.status != "delivered":
+    requests.post(
+        "https://hc-ping.com/" + hc_uuid + "/fail",
+        data="sid=" + message.sid + "\n" + "error=" + str(message.error_code),
+        timeout=10,
+    )
+else:
+    requests.post(
+        "https://hc-ping.com/" + hc_uuid, data="sid=" + message.sid, timeout=10
+    )
