0fb54712352525135837388e4e1af5c2e03e652e
Filippo Valsorda <hi@filippo.io>
2023-12-11 16:19:50 +0100

    Consolidate in a single file

--- cmd/benchdiff/benchdiff.go
+++ cmd/benchdiff/benchdiff.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -12,7 +14,7 @@ import (
 	"text/template"
 
 	"github.com/alecthomas/kong"
-	"github.com/willabides/benchdiff/cmd/benchdiff/internal"
+	"golang.org/x/crypto/sha3"
 )
 
 const defaultBenchArgsTmpl = `test {{ .Packages }} -run '^$'
@@ -199,13 +201,12 @@ func main() {
 	cacheDir, err := getCacheDir()
 	kctx.FatalIfErrorf(err)
 
-	bd := &internal.Benchdiff{
-		BenchCmd:   cli.BenchmarkCmd,
+	bd := &Benchdiff{
+		GoCmd:      cli.BenchmarkCmd,
 		BenchArgs:  benchArgs,
 		ResultsDir: cacheDir,
 		BaseRef:    cli.BaseRef,
 		HeadRef:    cli.HeadRef,
-		Path:       ".",
 		Writer:     os.Stdout,
 		Force:      cli.NoCache,
 		GitCmd:     cli.GitCmd,
@@ -225,4 +226,233 @@ func main() {
 	}
 	err = cmd.Run()
 	kctx.FatalIfErrorf(err)
+}
+
+type Benchdiff struct {
+	GoCmd      string
+	BenchArgs  string
+	ResultsDir string
+	BaseRef    string
+	HeadRef    string
+	GitCmd     string
+	Writer     io.Writer
+	Force      bool
+	Debug      *log.Logger
+}
+
+type RunResult struct {
+	HeadOutputFile string
+	BaseOutputFile string
+	BenchmarkCmd   string
+	HeadRef        string
+	BaseRef        string
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return !os.IsNotExist(err)
+	}
+	return true
+}
+
+func (c *Benchdiff) debug() *log.Logger {
+	if c.Debug == nil {
+		return log.New(io.Discard, "", 0)
+	}
+	return c.Debug
+}
+
+func (c *Benchdiff) cacheKey() string {
+	var b []byte
+	b = append(b, []byte(c.GoCmd)...)
+	b = append(b, []byte(c.BenchArgs)...)
+	b = append(b, []byte(os.Getenv("GOOS"))...)
+	b = append(b, []byte(os.Getenv("GOARCH"))...)
+	sum := sha3.Sum224(b)
+	return base64.RawURLEncoding.EncodeToString(sum[:])
+}
+
+// runCmd runs cmd sending its stdout and stderr to debug.Write()
+func runCmd(cmd *exec.Cmd, debug *log.Logger) error {
+	if debug == nil {
+		debug = log.New(io.Discard, "", 0)
+	}
+	var bufStderr bytes.Buffer
+	stderr := io.MultiWriter(&bufStderr, debug.Writer())
+	if cmd.Stderr != nil {
+		stderr = io.MultiWriter(cmd.Stderr, stderr)
+	}
+	cmd.Stderr = stderr
+	stdout := debug.Writer()
+	if cmd.Stdout != nil {
+		stdout = io.MultiWriter(cmd.Stdout, stdout)
+	}
+	cmd.Stdout = stdout
+	debug.Printf("+ %s", cmd)
+	err := cmd.Run()
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		err = fmt.Errorf(`error running command: %s
+exit code: %d
+stderr: %s`, cmd.String(), exitErr.ExitCode(), bufStderr.String())
+	}
+	return err
+}
+
+func (c *Benchdiff) runBenchmark(ref, filename string, force bool) error {
+	cmd := exec.Command(c.GoCmd, strings.Fields(c.BenchArgs)...)
+
+	stdlib := false
+	if rootPath, err := c.runGitCmd("rev-parse", "--show-toplevel"); err == nil {
+		// lib/time/zoneinfo.zip is a specific enough path, and it's here to
+		// stay because it's one of the few paths hardcoded into Go binaries.
+		zoneinfoPath := filepath.Join(string(rootPath), "lib", "time", "zoneinfo.zip")
+		if _, err := os.Stat(zoneinfoPath); err == nil {
+			stdlib = true
+			c.debug().Println("standard library detected")
+			cmd.Path = filepath.Join(string(rootPath), "bin", "go")
+		}
+	}
+
+	fileBuffer := &bytes.Buffer{}
+	if filename != "" {
+		c.debug().Printf("output file: %s", filename)
+		if ref != "" && !force {
+			if fileExists(filename) {
+				c.debug().Printf("+ skipping benchmark for ref %q because output file exists", ref)
+				return nil
+			}
+		}
+		cmd.Stdout = fileBuffer
+	}
+
+	if !stdlib {
+		goVersion, err := c.runGoCmd("env", "GOVERSION")
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(fileBuffer, "go: %s\n", goVersion)
+	}
+
+	var runErr error
+	if ref == "" {
+		runErr = runCmd(cmd, c.debug())
+	} else {
+		err := c.runAtGitRef(c.BaseRef, func(workPath string) {
+			if stdlib {
+				makeCmd := exec.Command(filepath.Join(workPath, "src", "make.bash"))
+				makeCmd.Dir = filepath.Join(workPath, "src")
+				makeCmd.Env = append(os.Environ(), "GOOS=", "GOARCH=")
+				runErr = runCmd(makeCmd, c.debug())
+				if runErr != nil {
+					return
+				}
+				cmd.Path = filepath.Join(workPath, "bin", "go")
+			}
+			cmd.Dir = workPath // TODO: add relative path of working directory
+			runErr = runCmd(cmd, c.debug())
+		})
+		if err != nil {
+			return err
+		}
+	}
+	if runErr != nil {
+		return runErr
+	}
+	if filename == "" {
+		return nil
+	}
+	return os.WriteFile(filename, fileBuffer.Bytes(), 0o666)
+}
+
+func (c *Benchdiff) Run() (result *RunResult, err error) {
+	if err := os.MkdirAll(c.ResultsDir, 0o700); err != nil {
+		return nil, err
+	}
+
+	headFlag := "--dirty"
+	if c.HeadRef != "" {
+		headFlag = c.HeadRef
+	}
+	headRef, err := c.runGitCmd("describe", "--tags", "--always", headFlag)
+	if err != nil {
+		return nil, err
+	}
+
+	baseRef, err := c.runGitCmd("describe", "--tags", "--always", c.BaseRef)
+	if err != nil {
+		return nil, err
+	}
+
+	baseFilename := fmt.Sprintf("benchdiff-%s-%s.out", baseRef, c.cacheKey())
+	baseFilename = filepath.Join(c.ResultsDir, baseFilename)
+
+	worktreeFilename := fmt.Sprintf("benchdiff-%s-%s.out", headRef, c.cacheKey())
+	worktreeFilename = filepath.Join(c.ResultsDir, worktreeFilename)
+
+	result = &RunResult{
+		BenchmarkCmd:   fmt.Sprintf("%s %s", c.GoCmd, c.BenchArgs),
+		HeadRef:        strings.TrimSpace(string(headRef)),
+		BaseRef:        strings.TrimSpace(string(baseRef)),
+		BaseOutputFile: baseFilename,
+		HeadOutputFile: worktreeFilename,
+	}
+
+	err = c.runBenchmark(c.BaseRef, baseFilename, c.Force)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.runBenchmark(c.HeadRef, worktreeFilename, c.Force)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *Benchdiff) runGoCmd(args ...string) ([]byte, error) {
+	var stdout bytes.Buffer
+	cmd := exec.Command(c.GoCmd, args...)
+	cmd.Stdout = &stdout
+	err := runCmd(cmd, c.debug())
+	return bytes.TrimSpace(stdout.Bytes()), err
+}
+
+func (c *Benchdiff) runGitCmd(args ...string) ([]byte, error) {
+	var stdout bytes.Buffer
+	cmd := exec.Command(c.GitCmd, args...)
+	cmd.Stdout = &stdout
+	err := runCmd(cmd, c.debug())
+	return bytes.TrimSpace(stdout.Bytes()), err
+}
+
+func (c *Benchdiff) runAtGitRef(ref string, fn func(path string)) error {
+	worktree, err := os.MkdirTemp("", "benchdiff")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		rErr := os.RemoveAll(worktree)
+		if rErr != nil {
+			fmt.Printf("Could not delete temp directory: %s\n", worktree)
+		}
+	}()
+
+	_, err = c.runGitCmd("worktree", "add", "--quiet", "--detach", worktree, ref)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_, cerr := c.runGitCmd("worktree", "remove", worktree)
+		if cerr != nil {
+			if exitErr, ok := cerr.(*exec.ExitError); ok {
+				fmt.Println(string(exitErr.Stderr))
+			}
+			fmt.Println(cerr)
+		}
+	}()
+	fn(worktree)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,13 +4,7 @@ go 1.20
 
 require (
 	github.com/alecthomas/kong v0.8.1
-	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.16.0
 )
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require golang.org/x/sys v0.15.0 // indirect
