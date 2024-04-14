8dc47720fc9798b36bb9db1d0a94f15e902befcc
Filippo Valsorda <hi@filippo.io>
2024-04-14 19:25:39 +0200

    Simplify command line parsing

--- benchdiff.go
+++ benchdiff.go
@@ -5,207 +5,89 @@ import (
 	"crypto/sha512"
 	"encoding/base64"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
+	"runtime/debug"
 	"strings"
-	"text/template"
 	"time"
-
-	"github.com/alecthomas/kong"
 )
 
-const defaultBenchArgsTmpl = `test -json {{ .Packages }} -run '^$'
-{{- if .Bench }} -bench {{ .Bench }}{{end}}
-{{- if .Count }} -count {{ .Count }}{{end}}
-{{- if .Benchtime }} -benchtime {{ .Benchtime }}{{end}}
-{{- if .CPU }} -cpu {{ .CPU }}{{ end }}
-{{- if .Tags }} -tags "{{ .Tags }}"{{ end }}
-{{- if .Benchmem }} -benchmem{{ end }}`
-
-var benchVars = kong.Vars{
-	"BenchCmdDefault":   `go`,
-	"CountHelp":         `Run each benchmark n times. If --cpu is set, run n times for each GOMAXPROCS value.'`,
-	"BenchHelp":         `Run only those benchmarks matching a regular expression. To run all benchmarks, use '--bench .'.`,
-	"BenchmarkArgsHelp": `Override the default args to the go command. This may be a template. See https://github.com/willabides/benchdiff for details."`,
-	"BenchtimeHelp":     `Run enough iterations of each benchmark to take t, specified as a time.Duration (for example, --benchtime 1h30s). The default is 1 second (1s). The special syntax Nx means to run the benchmark N times (for example, -benchtime 100x).`,
-	"PackagesHelp":      `Run benchmarks in these packages.`,
-	"BenchCmdHelp":      `The command to use for benchmarks.`,
-	"BenchstatCmdHelp":  `The command to use for benchstat.`,
-	"CacheDirHelp":      `Override the default directory where benchmark output is kept.`,
-	"BaseRefHelp":       `The git ref to be used as a baseline.`,
-	"HeadRefHelp":       `The git ref to be benchmarked. By default the worktree is used.`,
-	"NoCacheHelp":       `Rerun benchmarks even if the output already exists.`,
-	"GitCmdHelp":        `The executable to use for git commands.`,
-	"VersionHelp":       `Output the benchdiff version and exit.`,
-	"ClearCacheHelp":    `Remove benchdiff files from the cache dir.`,
-	"CPUHelp":           `Specify a list of GOMAXPROCS values for which the benchmarks should be executed. The default is the current value of GOMAXPROCS.`,
-	"BenchmemHelp":      `Memory allocation statistics for benchmarks.`,
-	"TagsHelp":          `Set the -tags flag on the go test command`,
-}
-
-var groupHelp = kong.Vars{
-	"gotestGroupHelp": "benchmark command line:",
-	"cacheGroupHelp":  "benchmark result cache:",
-}
-
-var cli struct {
-	Debug bool `kong:"help='write verbose output to stderr'"`
-
-	BaseRef      string `kong:"default=HEAD,help=${BaseRefHelp},group='x'"`
-	HeadRef      string `kong:"help=${BaseRefHelp},group='x'"`
-	GitCmd       string `kong:"default=git,help=${GitCmdHelp},group='x'"`
-	BenchstatCmd string `kong:"default=benchstat,help=${BenchstatCmdHelp},group='x'"`
-
-	Bench         string  `kong:"default='.',help=${BenchHelp},group='gotest'"`
-	BenchmarkArgs string  `kong:"placeholder='args',help=${BenchmarkArgsHelp},group='gotest'"`
-	BenchmarkCmd  string  `kong:"default=${BenchCmdDefault},help=${BenchCmdHelp},group='gotest'"`
-	Benchmem      bool    `kong:"help=${BenchmemHelp},group='gotest'"`
-	Benchtime     string  `kong:"help=${BenchtimeHelp},group='gotest'"`
-	Count         int     `kong:"default=10,help=${CountHelp},group='gotest'"`
-	CPU           CPUFlag `kong:"help=${CPUHelp},group='gotest',placeholder='GOMAXPROCS,...'"`
-	Packages      string  `kong:"default='./...',help=${PackagesHelp},group='gotest'"`
-	Tags          string  `kong:"help=${TagsHelp},group='gotest'"`
-
-	ClearCache ClearCacheFlag `kong:"help=${ClearCacheHelp},group='cache'"`
-	NoCache    bool           `kong:"help=${NoCacheHelp},group='cache'"`
-}
-
-// ClearCacheFlag flag for clearing cache
-type ClearCacheFlag bool
-
-// AfterApply clears cache
-func (v ClearCacheFlag) AfterApply(app *kong.Kong) error {
-	cacheDir, err := getCacheDir()
-	if err != nil {
-		return err
-	}
-	files, err := filepath.Glob(filepath.Join(cacheDir, "benchdiff-*.out"))
-	if err != nil {
-		return fmt.Errorf("error finding files in %s: %v", cacheDir, err)
-	}
-	for _, file := range files {
-		err = os.Remove(file)
-		if err != nil {
-			return fmt.Errorf("error removing %s: %v", file, err)
-		}
-	}
-	app.Exit(0)
-	return nil
-}
-
-func getCacheDir() (string, error) {
+func getCacheDir() string {
 	userCacheDir, err := os.UserCacheDir()
 	if err != nil {
-		return "", fmt.Errorf("error finding user cache dir: %v", err)
+		log.Fatalf("error finding user cache dir: %v", err)
 	}
-	return filepath.Join(userCacheDir, "benchdiff"), nil
+	return filepath.Join(userCacheDir, "benchdiff")
 }
-
-// CPUFlag is the flag for --cpu
-type CPUFlag []int
-
-func (c CPUFlag) String() string {
-	s := make([]string, len(c))
-	for i, cc := range c {
-		s[i] = strconv.Itoa(cc)
-	}
-	return strings.Join(s, ",")
-}
-
-func getBenchArgs() (string, error) {
-	argsTmpl := cli.BenchmarkArgs
-	if argsTmpl == "" {
-		argsTmpl = defaultBenchArgsTmpl
-	}
-	tmpl, err := template.New("").Parse(argsTmpl)
-	if err != nil {
-		return "", err
-	}
-	var benchArgs bytes.Buffer
-	err = tmpl.Execute(&benchArgs, cli)
-	if err != nil {
-		return "", err
-	}
-	args := benchArgs.String()
-	return args, nil
-}
-
-const description = `
-benchdiff runs go benchmarks on your current git worktree and a base ref then
-uses benchstat to show the delta.
-
-More documentation at https://github.com/willabides/benchdiff.
-`
 
 func main() {
-	userCacheDir, err := os.UserCacheDir()
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "error finding user cache dir: %v\n", err)
-		os.Exit(1)
+	clearCacheFlag := flag.Bool("clear-cache", false, "clear the cache")
+	baseRef := flag.String("base-ref", "HEAD", "base git ref")
+	headRef := flag.String("head-ref", "", "head git ref (defaults to worktree)")
+	debugFlag := flag.Bool("debug", false, "enable debug output")
+
+	flag.Parse()
+
+	if *clearCacheFlag {
+		cacheDir := getCacheDir()
+		files, err := filepath.Glob(filepath.Join(cacheDir, "benchdiff-*.out"))
+		if err != nil {
+			log.Fatalf("error finding files in %s: %v", cacheDir, err)
+		}
+		for _, file := range files {
+			err = os.Remove(file)
+			if err != nil {
+				log.Fatalf("error removing %s: %v", file, err)
+			}
+		}
+		os.Exit(0)
 	}
-	benchVars["CacheDirDefault"] = filepath.Join(userCacheDir, "benchdiff")
 
-	kctx := kong.Parse(&cli, benchVars, groupHelp,
-		kong.Description(strings.TrimSpace(description)),
-		kong.ExplicitGroups([]kong.Group{
-			{Key: "cache", Title: "benchmark result cache"},
-			{Key: "gotest", Title: "benchmark command line"},
-			{Key: "x"},
-		}),
-	)
-
-	benchArgs, err := getBenchArgs()
-	kctx.FatalIfErrorf(err)
-
-	cacheDir, err := getCacheDir()
-	kctx.FatalIfErrorf(err)
+	benchArgs := []string{"test", "-json", "-run", "^$", "-bench", ".", "-count", "10"}
+	benchArgs = append(benchArgs, flag.Args()...)
 
 	bd := &Benchdiff{
-		GoCmd:      cli.BenchmarkCmd,
 		BenchArgs:  benchArgs,
-		ResultsDir: cacheDir,
-		BaseRef:    cli.BaseRef,
-		HeadRef:    cli.HeadRef,
-		Force:      cli.NoCache,
-		GitCmd:     cli.GitCmd,
+		ResultsDir: getCacheDir(),
+		BaseRef:    *baseRef,
+		HeadRef:    *headRef,
+		Debug:      log.New(io.Discard, "", 0),
 	}
-	if cli.Debug {
+	if *debugFlag {
 		bd.Debug = log.New(os.Stderr, "", 0)
 	}
 	result, err := bd.Run()
-	kctx.FatalIfErrorf(err)
+	if err != nil {
+		log.Fatalf("error running benchmarks: %v", err)
+	}
 
-	cmd := exec.Command(cli.BenchstatCmd, result.BaseRef+"="+result.BaseOutputFile,
+	cmd := exec.Command("benchstat",
+		result.BaseRef+"="+result.BaseOutputFile,
 		result.HeadRef+"="+result.HeadOutputFile)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if cli.Debug {
-		bd.Debug.Printf("+ %s", cmd)
+	bd.Debug.Printf("+ %s", cmd)
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("error running benchstat: %v", err)
 	}
-	err = cmd.Run()
-	kctx.FatalIfErrorf(err)
 }
 
 type Benchdiff struct {
-	GoCmd      string
-	BenchArgs  string
+	BenchArgs  []string
 	ResultsDir string
 	BaseRef    string
 	HeadRef    string
-	GitCmd     string
-	Force      bool
 	Debug      *log.Logger
 }
 
 type RunResult struct {
 	HeadOutputFile string
 	BaseOutputFile string
-	BenchmarkCmd   string
 	HeadRef        string
 	BaseRef        string
 }
@@ -218,41 +100,36 @@ func fileExists(path string) bool {
 	return true
 }
 
-func (c *Benchdiff) debug() *log.Logger {
-	if c.Debug == nil {
-		return log.New(io.Discard, "", 0)
-	}
-	return c.Debug
-}
-
-// runCmd runs cmd sending its stdout and stderr to debug.Write()
 func runCmd(cmd *exec.Cmd, debug *log.Logger) error {
-	if debug == nil {
-		debug = log.New(io.Discard, "", 0)
-	}
 	var bufStderr bytes.Buffer
 	stderr := io.MultiWriter(&bufStderr, debug.Writer())
 	if cmd.Stderr != nil {
 		stderr = io.MultiWriter(cmd.Stderr, stderr)
 	}
 	cmd.Stderr = stderr
+
 	stdout := debug.Writer()
 	if cmd.Stdout != nil {
 		stdout = io.MultiWriter(cmd.Stdout, stdout)
 	}
 	cmd.Stdout = stdout
+
 	debug.Printf("+ %s", cmd)
 	err := cmd.Run()
 	if exitErr, ok := err.(*exec.ExitError); ok {
-		err = fmt.Errorf(`error running command: %s
-exit code: %d
-stderr: %s`, cmd.String(), exitErr.ExitCode(), bufStderr.String())
+		err = fmt.Errorf("error running command: %s\nexit code: %d\nstderr: \n%s", cmd.String(), exitErr.ExitCode(), bufStderr.String())
 	}
 	return err
 }
 
-func (c *Benchdiff) runBenchmark(ref, filename string, force bool) error {
-	cmd := exec.Command(c.GoCmd, strings.Fields(c.BenchArgs)...)
+func (c *Benchdiff) runBenchmark(ref, filename string) error {
+	c.Debug.Printf("output file: %s", filename)
+	if ref != "" && fileExists(filename) {
+		c.Debug.Printf("+ skipping benchmark for ref %q because output file exists", ref)
+		return nil
+	}
+
+	cmd := exec.Command("go", c.BenchArgs...)
 
 	stdlib := false
 	if rootPath, err := c.runGitCmd("rev-parse", "--show-toplevel"); err == nil {
@@ -261,7 +138,7 @@ func (c *Benchdiff) runBenchmark(ref, filename string, force bool) error {
 		zoneinfoPath := filepath.Join(string(rootPath), "lib", "time", "zoneinfo.zip")
 		if _, err := os.Stat(zoneinfoPath); err == nil {
 			stdlib = true
-			c.debug().Println("standard library detected")
+			c.Debug.Println("standard library detected")
 			cmd.Path = filepath.Join(string(rootPath), "bin", "go")
 		}
 	}
@@ -273,16 +150,6 @@ func (c *Benchdiff) runBenchmark(ref, filename string, force bool) error {
 		}
 	}}
 
-	if filename != "" {
-		c.debug().Printf("output file: %s", filename)
-		if ref != "" && !force {
-			if fileExists(filename) {
-				c.debug().Printf("+ skipping benchmark for ref %q because output file exists", ref)
-				return nil
-			}
-		}
-	}
-
 	if !stdlib {
 		goVersion, err := c.runGoCmd("env", "GOVERSION")
 		if err != nil {
@@ -293,21 +160,21 @@ func (c *Benchdiff) runBenchmark(ref, filename string, force bool) error {
 
 	var runErr error
 	if ref == "" {
-		runErr = runCmd(cmd, c.debug())
+		runErr = runCmd(cmd, c.Debug)
 	} else {
 		err := c.runAtGitRef(c.BaseRef, func(workPath string) {
 			if stdlib {
 				makeCmd := exec.Command(filepath.Join(workPath, "src", "make.bash"))
 				makeCmd.Dir = filepath.Join(workPath, "src")
 				makeCmd.Env = append(os.Environ(), "GOOS=", "GOARCH=")
-				runErr = runCmd(makeCmd, c.debug())
+				runErr = runCmd(makeCmd, c.Debug)
 				if runErr != nil {
 					return
 				}
 				cmd.Path = filepath.Join(workPath, "bin", "go")
 			}
 			cmd.Dir = workPath // TODO: add relative path of working directory
-			runErr = runCmd(cmd, c.debug())
+			runErr = runCmd(cmd, c.Debug)
 		})
 		if err != nil {
 			return err
@@ -316,17 +183,15 @@ func (c *Benchdiff) runBenchmark(ref, filename string, force bool) error {
 	if runErr != nil {
 		return runErr
 	}
-	if filename == "" {
-		return nil
-	}
 	return os.WriteFile(filename, fileBuffer.Bytes(), 0o666)
 }
 
 func (c *Benchdiff) countBenchmarks() (int, error) {
 	var count int
 
-	benchArgs := c.BenchArgs + " -benchtime 1ns"
-	cmd := exec.Command(c.GoCmd, strings.Fields(benchArgs)...)
+	benchArgs := append([]string(nil), c.BenchArgs...)
+	benchArgs = append(benchArgs, "-benchtime", "1ns", "-run", "^$")
+	cmd := exec.Command("go", benchArgs...)
 	cmd.Stdout = &TestJSONWriter{f: func(e *TestEvent) {
 		// Unfortunately, the go test -json output makes it hard to track timing
 		// output lines without heuristics. See https://go.dev/issue/66825.
@@ -336,7 +201,7 @@ func (c *Benchdiff) countBenchmarks() (int, error) {
 		}
 	}}
 
-	err := runCmd(cmd, c.debug())
+	err := runCmd(cmd, c.Debug)
 	return count, err
 }
 
@@ -371,23 +236,20 @@ func (c *Benchdiff) Run() (result *RunResult, err error) {
 	if err != nil {
 		return nil, err
 	}
-	c.debug().Printf("counted %d benchmarks", count)
+	c.Debug.Printf("counted %d benchmarks", count)
 
 	result = &RunResult{
-		BenchmarkCmd:   fmt.Sprintf("%s %s", c.GoCmd, c.BenchArgs),
 		HeadRef:        strings.TrimSpace(string(headRef)),
 		BaseRef:        strings.TrimSpace(string(baseRef)),
 		BaseOutputFile: baseFilename,
 		HeadOutputFile: headFilename,
 	}
 
-	err = c.runBenchmark(c.BaseRef, baseFilename, c.Force)
-	if err != nil {
+	if err := c.runBenchmark(c.BaseRef, baseFilename); err != nil {
 		return nil, err
 	}
 
-	err = c.runBenchmark(c.HeadRef, headFilename, c.Force)
-	if err != nil {
+	if err := c.runBenchmark(c.HeadRef, headFilename); err != nil {
 		return nil, err
 	}
 
@@ -405,8 +267,10 @@ func (c *Benchdiff) cacheFilename(ref string) (string, error) {
 	}
 
 	h := sha512.New()
-	fmt.Fprintf(h, "%s\n", c.GoCmd)
-	fmt.Fprintf(h, "%s\n", c.BenchArgs)
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		fmt.Fprintf(h, "%s\n", buildInfo.String())
+	}
+	fmt.Fprintf(h, "%q\n", c.BenchArgs)
 	fmt.Fprintf(h, "%s\n", env)
 	fmt.Fprintf(h, "%s\n", ref)
 	fmt.Fprintf(h, "%s\n", rootPath)
@@ -417,17 +281,17 @@ func (c *Benchdiff) cacheFilename(ref string) (string, error) {
 
 func (c *Benchdiff) runGoCmd(args ...string) ([]byte, error) {
 	var stdout bytes.Buffer
-	cmd := exec.Command(c.GoCmd, args...)
+	cmd := exec.Command("go", args...)
 	cmd.Stdout = &stdout
-	err := runCmd(cmd, c.debug())
+	err := runCmd(cmd, c.Debug)
 	return bytes.TrimSpace(stdout.Bytes()), err
 }
 
 func (c *Benchdiff) runGitCmd(args ...string) ([]byte, error) {
 	var stdout bytes.Buffer
-	cmd := exec.Command(c.GitCmd, args...)
+	cmd := exec.Command("git", args...)
 	cmd.Stdout = &stdout
-	err := runCmd(cmd, c.debug())
+	err := runCmd(cmd, c.Debug)
 	return bytes.TrimSpace(stdout.Bytes()), err
 }
 
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/willabides/benchdiff
 
-go 1.20
-
-require github.com/alecthomas/kong v0.8.1
+go 1.22.2
