ef928cdb2a9a909aa7020bb1778b55d7ac542f56
Filippo Valsorda <hi@filippo.io>
2024-08-18 22:24:38 +0200

    c2sp.org: support versioned URLs

--- filippo.fly.dev/c2sp.go
+++ filippo.fly.dev/c2sp.go
@@ -9,6 +9,7 @@ import (
 
 func c2sp(mux *http.ServeMux) {
 	specRe := regexp.MustCompile(`^/([A-Za-z0-9-]+)$`)
+	versRe := regexp.MustCompile(`^/([A-Za-z0-9-]+)@(v[a-z0-9-.]+)$`)
 	cctvRe := regexp.MustCompile(`^/CCTV/([A-Za-z0-9-]+)$`)
 
 	handleFuncWithCounter(mux, "c2sp.org/", func(w http.ResponseWriter, r *http.Request) {
@@ -36,6 +37,11 @@ func c2sp(mux *http.ServeMux) {
 
 		if match := cctvRe.FindStringSubmatch(r.URL.Path); match != nil {
 			http.Redirect(w, r, "https://github.com/C2SP/CCTV/tree/main/"+match[1], http.StatusFound)
+			return
+		}
+
+		if match := versRe.FindStringSubmatch(r.URL.Path); match != nil {
+			http.Redirect(w, r, "https://github.com/C2SP/C2SP/blob/"+match[1]+"/"+match[2]+"/"+match[1]+".md", http.StatusFound)
 			return
 		}
 
