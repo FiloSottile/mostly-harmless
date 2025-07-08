89b9d769bd38a51eea21e188849afc85cacc5046
Filippo Valsorda <hi@filippo.io>
2025-07-08 02:52:55 +0200

    filippo.io: test draft exclusion and "private" inclusion

--- filippo.fly.dev/buttondown.go
+++ filippo.fly.dev/buttondown.go
@@ -29,7 +29,6 @@ type buttondownEmail struct {
 	Slug        string        `json:"slug"`
 	Subject     string        `json:"subject"`
 	Status      string        `json:"status"`
-	EmailType   string        `json:"email_type"`
 	Metadata    struct {
 		OverrideSlug string `json:"override_slug"`
 		OverrideGUID string `json:"override_guid"`
@@ -297,10 +296,6 @@ func fetchMails() error {
 	}
 
 	allEmails = slices.DeleteFunc(allEmails, func(e *buttondownEmail) bool {
-		// if e.EmailType != "public" {
-		// 	log.Printf("hiding email %q with type %q", e.ID, e.EmailType)
-		// 	return true
-		// }
 		switch e.Status {
 		case "draft", "scheduled", "managed_by_rss", "deleted", "paused", "errored", "transactional":
 			return true
