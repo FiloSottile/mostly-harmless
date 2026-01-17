d8f7360db67ca395a7a033842ef3ae9dbdfc8191
Filippo Valsorda <hi@filippo.io>
2026-01-17 20:30:02 +0100

    frood: don't power off for inactivity for an hour after boot

--- frood/root/etc/periodic/15min/inactive.sh
+++ frood/root/etc/periodic/15min/inactive.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -eu
 
+if [ "$(cut -d' ' -f1 /proc/uptime | cut -d'.' -f1)" -lt 3600 ]; then
+    exit 0
+fi
+
 for name in sshd-session tmux ksmbd:; do
     if pgrep "$name" >/dev/null 2>&1; then
         exit 0
