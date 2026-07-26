cf856698886324ab1c781de4a8c351b63cb94abc
Filippo Valsorda <hi@filippo.io>
2026-07-26 19:56:01 +0200

    public-config: add support for trastevere server

--- public-config/public-config.go
+++ public-config/public-config.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
-var configFiles = []string{
+var bothConfigFiles = []string{
 	"/etc/sunlight/sunlight.yaml",
 	"/etc/sunlight/sunlight-staging.yaml",
 	"/etc/sunlight/skylight.yaml",
@@ -20,22 +22,38 @@ var configFiles = []string{
 	"/etc/systemd/system/partial-aftersun.timer",
 	"/etc/systemd/system/partial-aftersun-staging.service",
 	"/etc/systemd/system/partial-aftersun-staging.timer",
-	"/etc/systemd/system/heliograph-dashboard.service",
-	"/etc/systemd/system/heliograph-dashboard.timer",
-	"/etc/systemd/system/age-keyserver.service",
-	"/etc/systemd/system/pkg-geomys-dev.service",
-	"/etc/systemd/system/plc-replica.service",
 	"/etc/systemd/system/public-config.service",
 	"/etc/logrotate.d/jsonl",
 	"/etc/caddy/Caddyfile",
 	"/etc/systemd/system/caddy.service.d/override.conf",
-	"/etc/prometheus/prometheus.yml",
 	"/usr/local/bin/zfs-textfile",
 	"/etc/systemd/system/zfs-textfile.service",
 	"/etc/systemd/system/zfs-textfile.timer",
 }
 
+var tuscoloConfigFiles = []string{
+	"/etc/systemd/system/heliograph-dashboard.service",
+	"/etc/systemd/system/heliograph-dashboard.timer",
+	"/etc/systemd/system/heliograph-dashboard-trastevere.service",
+	"/etc/systemd/system/heliograph-dashboard-trastevere.timer",
+	"/etc/systemd/system/age-keyserver.service",
+	"/etc/systemd/system/pkg-geomys-dev.service",
+	"/etc/systemd/system/plc-replica.service",
+	"/etc/prometheus/prometheus.yml",
+}
+
 func main() {
+	nameFlag := flag.String("name", "", "Name of the server to display configuration for (tuscolo or trastevere)")
+	flag.Parse()
+	var configFiles []string
+	switch *nameFlag {
+	case "tuscolo":
+		configFiles = append(bothConfigFiles, tuscoloConfigFiles...)
+	case "trastevere":
+		configFiles = bothConfigFiles
+	default:
+		log.Fatalf("Unknown server name: %s", *nameFlag)
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		type File struct {
@@ -51,7 +69,13 @@ func main() {
 			}
 			files = append(files, File{Path: path, Contents: string(contents)})
 		}
-		tmpl.Execute(w, files)
+		tmpl.Execute(w, struct {
+			Files []File
+			Name  string
+		}{
+			Files: files,
+			Name:  strings.Title(*nameFlag),
+		})
 	})
 	server := &http.Server{
 		Addr:         "localhost:8080",
@@ -71,7 +95,7 @@ var tmpl = template.Must(template.New("config").Parse(`
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="canonical" href="https://config.sunlight.geomys.org/">
-    <title>Geomys Tuscolo CT Log Server Public Configuration</title>
+    <title>Geomys {{ .Name }} CT Log Server Public Configuration</title>
     <style>
         :root {
             font-family: Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif;
@@ -106,17 +130,17 @@ var tmpl = template.Must(template.New("config").Parse(`
 </head>
 <body>
 <div class="container">
-    <h1>Geomys Tuscolo CT Log Server Public Configuration</h1>
+    <h1>Geomys {{ .Name }} CT Log Server Public Configuration</h1>
     <p>
         This is the <em>live</em> public configuration for the
         <a href="https://groups.google.com/a/chromium.org/g/ct-policy/c/KCzYEIIZSxg/m/zD26fYw4AgAJ">
-        Geomys Tuscolo CT Log Server</a>, a <a href="https://sunlight.dev/">Sunlight</a> instance.
+        Geomys {{ .Name }} CT Log Server</a>, a <a href="https://sunlight.dev/">Sunlight</a> instance.
 
     <p>
         See also the <a href="https://docs.google.com/document/d/1ID8dX5VuvvrgJrM0Re-jt6Wjhx1eZp-trbpSIYtOhRE/edit?tab=t.0#heading=h.v39dw5r67cif">
         public playbooks</a>.
 
-    {{ range . }}
+    {{ range .Files }}
         <h2 id="{{ .Path }}"><a href="#{{ .Path }}">{{ .Path }}</a></h2>
         <pre><code>{{ .Contents }}</code></pre>
     {{ end }}
